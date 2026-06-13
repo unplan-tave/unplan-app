9e9861dffb4bebadfe077a78ca940a28aeb1b33d
haewonwon <khcw1029@daum.net>
2026-06-13 15:41:26 +0900

    feat: 로그인 화면 UI 갱신

--- src/components/ui/Footer/HomeIndicator.tsx
+++ src/components/ui/Footer/HomeIndicator.tsx
@@ -1,30 +1,14 @@
 import { Platform, StyleSheet, View } from 'react-native';
 
 export function HomeIndicator() {
-  if (Platform.OS === 'android') {
-    return <View style={styles.androidSpace} />;
+  if (Platform.OS === 'ios') {
+    return null;
   }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.indicator} />
-    </View>
-  );
+  return <View style={styles.androidSpace} />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: 34,
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    paddingBottom: 8,
-  },
-  indicator: {
-    width: 144,
-    height: 5,
-    borderRadius: 100,
-    backgroundColor: '#000000',
-  },
   androidSpace: {
     height: 16,
   },
