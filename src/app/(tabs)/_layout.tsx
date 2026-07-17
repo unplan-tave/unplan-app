e34573e6dd4c4bce6f4ce95fdab0d6e1185df66f
haewonwon <khcw1029@daum.net>
2026-07-18 04:23:21 +0900

    chore: settings UI primitives 카탈로그 화면 제거

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -31,13 +31,6 @@ export default function TabsLayout() {
           tabBarLabel: '설정',
         }}
       />
-      <Tabs.Screen
-        name="settings-ui-primitives"
-        options={{
-          href: null,
-          title: 'UI 컴포넌트',
-        }}
-      />
     </Tabs>
   );
 }
